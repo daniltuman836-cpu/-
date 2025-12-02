2f849d231b853995d7add4592acc22392de97042
daniltuman836-cpu <daniltuman836@gmail.com>
2025-12-02 19:44:24 +0200

    Singleton

---
+++ MazeSingleton/MazeSingleton.cs
@@ -0,0 +1,51 @@
+﻿using AbstractFactory;
+using MazeLibrary;
+using MazeLibrary.Doors;
+using MazeLibrary.Rooms;
+using MazeLibrary.Walls;
+
+namespace MazeSingleton
+{
+    public class MazeSingleton : IMazeFactory
+    {
+        private static IMazeFactory _instance = null;
+
+        private MazeSingleton() { }
+
+        public static IMazeFactory Instance()
+        {
+            if (_instance == null)
+            {
+                Console.WriteLine("Вы создали единственный экземпляр лабиринта");
+                _instance = new MazeSingleton();
+            }
+            Console.WriteLine("Экземпляр лабиринта уже существует");
+            return _instance;
+        }
+
+        public Maze MakeMaze()
+        {
+            Console.WriteLine("Вы создали обычный лабиринт");
+            return new Maze();
+        }
+
+        public Wall MakeWall()
+        {
+            return new Wall();
+        }
+
+        public Room MakeRoom(int number)
+        {
+            if (number <= 0)
+            {
+                throw new ArgumentException("Номер комнаты должен быть натуральным числом");
+            }
+            return new Room(number);
+        }
+
+        public Door MakeDoor(Room room1, Room room2)
+        {
+            return new Door(room1, room2);
+        }
+    }
+}
---
+++ ProgramSingleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using AbstractFactory;
+using MazeLibrary.Rooms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeSingleton
+{
+    class ProgramSingleton
+    {
+        static void Main(string[] args)
+        {
+            MazeGame game = new MazeGame();
+            IMazeFactory maze = MazeSingleton.Instance();
+            game.CreateMaze(maze);
+            IMazeFactory mazeSingletonTwo = MazeSingleton.Instance();
+        }
+    }
+}
